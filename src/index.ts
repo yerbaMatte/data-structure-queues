c14fbb6c4582026455bce446252c4dd5887a3970
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-03-07 21:20:27 +0100

    enqueue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+type T = number | string;
+type node = _Node<T> | null;
+
 class _Node<T> {
   value: T;
   next: _Node<T> | null;
@@ -5,6 +8,39 @@ class _Node<T> {
   constructor(val: T) {
     this.value = val;
     this.next = null;
+  }
+}
+
+class Queue {
+  first: node;
+  last: node;
+  size: number;
+
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val: T) {
+    const newNode = new _Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last!.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (this.size === 0) return null;
+    const curr = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first!.next;
   }
 }
 
